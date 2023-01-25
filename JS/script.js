264db0730d75a220b205324b30b388a6ce071d67
matteo <matteo.battilani20@gmail.com>
2023-01-25 09:08:07 +0100

    html and js refactoring

--- JS/script.js
+++ JS/script.js
@@ -39,94 +39,90 @@ button.addEventListener("click", (event) => {
         }
       }
       // Se ci sono città omonime mostro all'utente una lista di tali città in modo che scelga quella corretta
-      if (linksCount > 1) {
-        let html = "<ol class='list'>";
-        const promises = [];
-        for (let i = 0; i < searchResults.length; i++) {
-          promises.push(axios.get(searchResults[i]._links["city:item"].href));
-        }
-        Promise.all(promises).then((responses) => {
-          for (let i = 0; i < responses.length; i++) {
-            if (responses[i].data._links.hasOwnProperty("city:urban_area")) {
-              html += `<li class='list-item' data-href='${searchResults[i]._links["city:item"].href}'>${searchResults[i].matching_full_name}</li>`;
-            }
-          }
-          html += "</ol>";
-          if (html === "<ol class='list'></ol>") {
-            html =
-              '<div class="alert alert-warning" role="alert">City not found in database</div>';
-          }
-          scoringContainer.innerHTML = html;
-        });
 
-        scoringContainer.addEventListener("click", (event) => {
-          if (event.target.tagName === "LI") {
-            axios
-              .get(event.target.getAttribute("data-href"))
-              .then((response) => {
-                // NUOVA CHIAMATA API
-                const urbanAreaUrl =
-                  response.data._links["city:urban_area"].href;
-                axios
-                  .get(urbanAreaUrl)
-                  .then((response) => {
-                    const data = response.data;
-                    console.log(data);
-                    const scoresUrl = response.data._links["ua:scores"].href;
-                    axios
-                      .get(scoresUrl)
-                      .then((response) => {
-                        const scoresData = response.data;
-                        // Fai qualcosa con i dati delle valutazioni
-                        let categories = response.data.categories;
-                        let summary = response.data.summary;
-                        let cityScore = response.data.teleport_city_score;
-                        for (let i = 0; i < categories.length; i++) {
-                          let htmlDescription = `<p>${summary}</p> 
+      let html = "<ol class='list'>";
+      const promises = [];
+      for (let i = 0; i < searchResults.length; i++) {
+        promises.push(axios.get(searchResults[i]._links["city:item"].href));
+      }
+      Promise.all(promises).then((responses) => {
+        for (let i = 0; i < responses.length; i++) {
+          if (responses[i].data._links.hasOwnProperty("city:urban_area")) {
+            html += `<li class='list-item' data-href='${searchResults[i]._links["city:item"].href}'>${searchResults[i].matching_full_name}</li>`;
+          }
+        }
+        html += "</ol>";
+        if (html === "<ol class='list'></ol>") {
+          html =
+            '<div class="alert alert-warning" role="alert">City not found in database</div>';
+        }
+        scoringContainer.innerHTML = html;
+      });
+
+      scoringContainer.addEventListener("click", (event) => {
+        if (event.target.tagName === "LI") {
+          axios
+            .get(event.target.getAttribute("data-href"))
+            .then((response) => {
+              // NUOVA CHIAMATA API
+              const urbanAreaUrl = response.data._links["city:urban_area"].href;
+              axios
+                .get(urbanAreaUrl)
+                .then((response) => {
+                  const data = response.data;
+                  console.log(data);
+                  const scoresUrl = response.data._links["ua:scores"].href;
+                  axios
+                    .get(scoresUrl)
+                    .then((response) => {
+                      const scoresData = response.data;
+                      // Fai qualcosa con i dati delle valutazioni
+                      let categories = response.data.categories;
+                      let summary = response.data.summary;
+                      let cityScore = response.data.teleport_city_score;
+                      for (let i = 0; i < categories.length; i++) {
+                        let htmlDescription = `<p>${summary}</p> 
                           <p style='text-align: center;'><b>City Scoring: ${cityScore.toFixed(
                             2
                           )}<b></p>`;
-                          let html = "<div class='categories'>";
-                          for (let i = 0; i < categories.length; i++) {
-                            let score = categories[i].score_out_of_10;
-                            html += `<div><span class='dot' style='background-color: ${categories[i].color};'></span>
+                        let html = "<div class='categories'>";
+                        for (let i = 0; i < categories.length; i++) {
+                          let score = categories[i].score_out_of_10;
+                          html += `<div><span class='dot' style='background-color: ${categories[i].color};'></span>
                               <span>${categories[i].name}</span>`;
-                            if (Number.isInteger(score)) {
-                              html += `<p>${score}/10</p>
+                          if (Number.isInteger(score)) {
+                            html += `<p>${score}/10</p>
                               </div>`;
-                            } else {
-                              html += `
+                          } else {
+                            html += `
                               <p>${score.toFixed(1)}/10</p>
                               </div>`;
-                            }
                           }
-                          html += "</div>";
-                          cityDescriptionContainer.classList.add(
-                            "border-container",
-                            "windows-background",
-                            "paragraph"
-                          );
-                          cityDescriptionContainer.innerHTML = htmlDescription;
-                          scoringContainer.innerHTML = html;
                         }
-                        console.log(scoresData);
-                      })
-                      .catch((error) => {
-                        console.error(error);
-                      });
-                  })
-                  .catch((error) => {
-                    console.error(error);
-                  });
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-          }
-        });
-      } else {
-        // SE NON CI SONO CITTà OMONIME ALLORA DEVO MOSTRARE SEMPLICEMENTE I DATI
-      }
+                        html += "</div>";
+                        cityDescriptionContainer.classList.add(
+                          "border-container",
+                          "windows-background",
+                          "paragraph"
+                        );
+                        cityDescriptionContainer.innerHTML = htmlDescription;
+                        scoringContainer.innerHTML = html;
+                      }
+                      console.log(scoresData);
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      });
     })
     .catch((error) => {
       console.error(error);
